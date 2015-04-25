882371515b961d38f803b2eeb461b0f7a617e2a7
Jethro Muller <mlljet001@myuct.ac.za>
2015-04-25 18:28:38 +0200

    Add iterator class.

--- src/Image.h
+++ src/Image.h
@@ -12,7 +12,7 @@ public:
 
     int width;
     int height;
-    std::unique_ptr<unsigned char **> imageData;
+    std::unique_ptr<unsigned char []> imageData;
 
     // Copy Constructor
     Image(const Image &rhs) {
@@ -32,22 +32,60 @@ public:
     }
 
     Image operator+(const Image & image) {
-        Image result;
-        result.width = this->width;
-        result.height = this->height;
-        unsigned char ** newImageData = new unsigned char *[height];
 
-        for (int row = 0; row < height; ++row) {
-            newImageData[row] = new unsigned char[width];
-            for (int col = 0; col < width; ++col) {
-                newImageData[row][col] = *image.imageData.get()[row][col] + *this->imageData.get()[row][col];
-            }
-        }
-//        std::unique_ptr<unsigned char **> newPtr(newImageData);
-//        imageData = std::move(newPtr);
-
-        return result;
     }
+
+    iterator begin() {
+        return iterator(imageData.get());
+    }
+
+    iterator end() {
+        return iterator(imageData.get(), width * height);
+    }
+
+    class iterator {
+    private:
+        unsigned char * pointer;
+        int counter;
+    public:
+        iterator(unsigned char * ptr): pointer(ptr), counter(0) {}
+        iterator(unsigned char * ptr, int count): pointer(ptr), counter(count) {}
+        iterator(const iterator& copyIter) : pointer(copyIter.pointer) {}
+        iterator& operator++() {
+            ++counter;
+            if (counter > 255) {
+                counter = 255;
+            }
+            return *this;
+        }
+        iterator operator++(int) {
+            iterator tempIter(*this);
+            operator++();
+            return tempIter;
+        }
+
+        iterator& operator--() {
+            --counter;
+            if (counter < 0) {
+                counter = 0;
+            }
+            return *this;
+        }
+        iterator operator--(int) {
+            iterator tempIter(*this);
+            operator--();
+            return tempIter;
+        }
+
+        unsigned char& operator*() {
+            return pointer[counter];
+        }
+
+        iterator & operator=(const iterator & rhs) {
+            iterator newIter(std::move(rhs.pointer), rhs.counter);
+            return newIter;
+        }
+    };
 };
 
 
