5b9bf767e096157c0294aeb26450c883b31bb7c6
Jethro Muller <JethroMuller@gmail.com>
2015-04-26 22:07:26 +0200

    Implement basic class outline. Add image file loading.

--- src/Image.cpp
+++ src/Image.cpp
@@ -4,25 +4,47 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-Image::Image(std::string fileName) {
-    std::ifstream imgFile(fileName, std::ios::binary);
-
-    unsigned char **imageBytes = new unsigned char *[height];
-    string line;
-    while (!imgFile.eof()) {
-        getline(imgFile, line);
-        cout << line << endl;
+namespace MLLJET001 {
+    Image::~Image() {
+        // TODO delete [] imageData.get();
     }
 
-    imgFile.close();
+    void Image::save() {
+        // TODO Write file out
+    }
 
-    imageData = imgFile;
-}
+    void Image::load() {
+        std::ifstream imgFile(filename, std::ios::binary);
+        if (!imgFile.is_open()) {
+            cout << "File " << filename << " not found" << endl;
+            exit(0);
+        }
 
+        string line;
+        while (!imgFile.eof()) {
+            getline(imgFile, line);
+            if (line[0] == '#' || line == "P5") {
+                continue;
+            } else if (line == "255") {
+                break;
+            } else {
+                stringstream ss(line);
+                string num;
+                getline(ss, num, ' ');
+                width = stoi(num);
+                getline(ss, num, ' ');
+                height = stoi(num);
+                cout << width << " " << height << endl;
+            }
+        }
+        unsigned char *imageBytes = new unsigned char [height*width];
+        imgFile.read((char *) imageBytes, height * width);
+        imgFile.close();
 
-Image::~Image() {
-
+        imageData = unique_ptr<unsigned char []>(imageBytes);
+    }
 }
