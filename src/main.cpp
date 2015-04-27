326b1ba79966f2132e053732081f89f38fb16d6f
Jethro Muller <JethroMuller@gmail.com>
2015-04-27 17:16:24 +0200

    Switch to using << and >> instead of load and save.

--- src/main.cpp
+++ src/main.cpp
@@ -15,40 +15,37 @@ int main(int argc,  const char* argv[]) {
 
     string image1Name = argv[2];
     MLLJET001::Image img1;
-    img1.load(image1Name);
+    img1 << image1Name;
 
     string outputName = argv[argc-1];
     MLLJET001::Image imgOut;
 
     if (command == "-a") { // Add
         MLLJET001::Image img2;
-        img2.load(argv[3]);
+        img2 << argv[3];
 
         imgOut = img1 + img2;
-        imgOut.save(outputName);
     } else if (command == "-s") { // Subtract
         MLLJET001::Image img2;
-        img2.load(argv[3]);
+        img2 << argv[3];
 
         imgOut = img1 - img2;
-        imgOut.save(outputName);
     } else if (command == "-i") { // Invert
         imgOut = !img1;
-        imgOut.save(outputName);
+        imgOut >> outputName;
     } else if (command == "-l") { // Mask
         MLLJET001::Image img2;
-        img2.load(argv[3]);
+        img2 << argv[3];
 
         imgOut = img1 / img2;
-        imgOut.save(outputName);
     } else if (command == "-t") { // Threshold
         int threshold = stoi(argv[3]);
 
         imgOut = img1 * threshold;
-        imgOut.save(outputName);
     } else {
         cout << "Command: " << command << " not recognized.";
+        return 0;
     }
-
+    imgOut >> outputName;
     return 0;
 }
